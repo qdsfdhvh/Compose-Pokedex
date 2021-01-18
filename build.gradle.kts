1c7681b7e190877778776638c9596bfeed89474b
seiko <605590140@qq.com>
2021-01-18 14:19:45 +0800

    Repositories applyDefault

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,16 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 buildscript {
-    repositories {
-        google()
-        jcenter()
-    }
+    repositories.applyDefault()
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-alpha04")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
 allprojects {
-    repositories {
-        google()
-        jcenter()
+    repositories.applyDefault()
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 }
---
+++ buildSrc/src/main/kotlin/Extensions.kt
@@ -0,0 +1,20 @@
+import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.kotlin.dsl.maven
+import org.gradle.plugin.use.PluginDependenciesSpec
+import org.gradle.plugin.use.PluginDependencySpec
+
+val PluginDependenciesSpec.androidApplication: PluginDependencySpec
+  get() = id("com.android.application")
+
+val PluginDependenciesSpec.androidLibrary: PluginDependencySpec
+  get() = id("com.android.library")
+
+val PluginDependenciesSpec.daggerHilt: PluginDependencySpec
+  get() = id("dagger.hilt.android.plugin")
+
+fun RepositoryHandler.applyDefault() {
+  google()
+  jcenter()
+  mavenCentral()
+  maven("https://dl.bintray.com/kotlin/kotlin-eap")
+}
