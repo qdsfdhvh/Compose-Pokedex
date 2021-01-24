409857aab1bc5773dd8761584985296d94166329
seiko <605590140@qq.com>
2021-01-24 21:59:07 +0800

    Update compose Type.kt

--- common/compose/src/main/java/com/seiko/common/compose/theme/Type.kt
+++ common/compose/src/main/java/com/seiko/common/compose/theme/Type.kt
@@ -2,27 +2,47 @@ package com.seiko.common.compose.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val typography = Typography(
+  h4 = TextStyle(
+    fontWeight = FontWeight.W600,
+    fontSize = 30.sp
+  ),
+  h5 = TextStyle(
+    fontWeight = FontWeight.W600,
+    fontSize = 24.sp
+  ),
+  h6 = TextStyle(
+    fontWeight = FontWeight.W600,
+    fontSize = 20.sp
+  ),
+  subtitle1 = TextStyle(
+    fontWeight = FontWeight.W600,
+    fontSize = 16.sp
+  ),
+  subtitle2 = TextStyle(
+    fontWeight = FontWeight.W500,
+    fontSize = 14.sp
+  ),
   body1 = TextStyle(
-    fontFamily = FontFamily.Default,
     fontWeight = FontWeight.Normal,
     fontSize = 16.sp
+  ),
+  body2 = TextStyle(
+    fontSize = 14.sp
+  ),
+  button = TextStyle(
+    fontWeight = FontWeight.W500,
+    fontSize = 14.sp
+  ),
+  caption = TextStyle(
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp
+  ),
+  overline = TextStyle(
+    fontWeight = FontWeight.W500,
+    fontSize = 12.sp
   )
-  /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
