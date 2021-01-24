0dad8116f37ca6abbc459155c3d8da73041d7e7e
seiko <605590140@qq.com>
2021-01-24 22:03:52 +0800

    HomeScene PokemonList

--- feature/home/src/main/java/com/seiko/feature/home/HomeScene.kt
+++ feature/home/src/main/java/com/seiko/feature/home/HomeScene.kt
@@ -1,11 +1,102 @@
 package com.seiko.feature.home
 
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.layout.ConstraintLayout
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.seiko.common.compose.extensions.navViewModel
+import com.seiko.common.compose.theme.ComposePokedexTheme
+import com.seiko.common.compose.util.ThemedPreview
+import com.seiko.common.compose.widget.NetworkImage
+import com.seiko.common.compose.widget.StaggeredVerticalGrid
+import com.seiko.data.model.Pokemon
 
 @Composable
 fun HomeScene() {
   val vm = navViewModel<HomeViewModel>()
-  Text(text = "Home")
+  val pokemonList: List<Pokemon> by vm.pokemonList.observeAsState(emptyList())
+  ComposePokedexTheme {
+    HomePokemonList(pokemonList)
+  }
+}
+
+@Composable
+fun HomePokemonList(
+  pokemonList: List<Pokemon>,
+) {
+  ScrollableColumn {
+    StaggeredVerticalGrid(
+      maxColumnWidth = 220.dp,
+      modifier = Modifier.padding(4.dp)
+    ) {
+      pokemonList.forEach { pokemon ->
+        HomePokemon(pokemon = pokemon)
+      }
+    }
+  }
+}
+
+@Composable
+fun HomePokemon(
+  pokemon: Pokemon,
+  modifier: Modifier = Modifier,
+) {
+  Surface(
+    modifier = modifier.padding(4.dp),
+    color = MaterialTheme.colors.background,
+    elevation = 8.dp,
+    shape = RoundedCornerShape(8.dp)
+  ) {
+    ConstraintLayout {
+      val (image, title) = createRefs()
+
+      NetworkImage(
+        url = pokemon.imageUrl,
+        modifier = Modifier
+          .constrainAs(image) {
+            centerHorizontallyTo(parent)
+            top.linkTo(parent.top)
+          }
+          .aspectRatio(0.8f),
+        contentScale = ContentScale.Inside
+      )
+
+      Text(
+        text = pokemon.name,
+        style = MaterialTheme.typography.h6,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+          .constrainAs(title) {
+            centerHorizontallyTo(parent)
+            top.linkTo(image.bottom)
+            bottom.linkTo(parent.bottom)
+          }
+          .padding(8.dp)
+      )
+    }
+  }
+}
+
+@Composable
+@Preview
+fun HomePokemonPreview() {
+  ThemedPreview {
+    HomePokemon(pokemon = Pokemon(
+      page = 1,
+      name = "bulbasaur",
+      url = "https://pokeapi.co/api/v2/pokemon/1/"
+    ))
+  }
 }
--- feature/home/src/main/java/com/seiko/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/seiko/feature/home/HomeViewModel.kt
@@ -1,10 +1,30 @@
 package com.seiko.feature.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.seiko.data.model.Pokemon
+import com.seiko.data.repository.PokemonListRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+  private val pokemonListRepository: PokemonListRepository
+) : ViewModel() {
 
+  val pokemonList: LiveData<List<Pokemon>>
+
+  init {
+    Timber.d("$this is created")
+
+    pokemonList = pokemonListRepository.fetchPokemonList(0)
+      .asLiveData()
+  }
+
+  override fun onCleared() {
+    super.onCleared()
+    Timber.d("$this is clear")
+  }
 }
