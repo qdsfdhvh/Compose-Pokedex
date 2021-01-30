8186ea29422c3c0fcd149efd69c1e9b5a6d84ec9
seiko <605590140@qq.com>
2021-01-30 11:42:41 +0800

    Update root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,29 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
-    repositories.applyDefault()
-    dependencies {
-        classpath(Plugin.androidGradle)
-        classpath(Plugin.kotlinGradle)
-        classpath(Plugin.hiltGradle)
-    }
+  repositories.applyDefault()
+  dependencies {
+    classpath(Plugin.androidGradle)
+    classpath(Plugin.kotlinGradle)
+    classpath(Plugin.hiltGradle)
+  }
 }
 
 allprojects {
-    repositories.applyDefault()
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+  repositories.applyDefault()
+  tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+  }
+}
+
+subprojects {
+  tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+      useIR = true
+      freeCompilerArgs = listOf(
+        "-Xallow-jvm-ir-dependencies",
+        "-Xskip-prerelease-check"
+      )
     }
+  }
 }
