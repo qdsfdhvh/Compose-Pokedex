907940e5edcb5a7dab1ef426fb00d25d5bb692e1
seiko <605590140@qq.com>
2021-01-31 09:16:51 +0800

    catch error with HomeViewModel

--- feature/home/src/main/java/com/seiko/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/seiko/feature/home/HomeViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.asLiveData
 import com.seiko.base.mapper.mapToList
 import com.seiko.data.repository.PokemonListRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import timber.log.Timber
 import javax.inject.Inject
@@ -24,6 +25,7 @@ class HomeViewModel @Inject constructor(
     pokemonList = pokemonListRepository
       .fetchPokemonList(0)
       .map { mapper.mapToList(it) }
+      .catch { Timber.w(it) }
       .asLiveData()
   }
 
